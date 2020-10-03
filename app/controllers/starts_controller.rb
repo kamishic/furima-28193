3dafc0cc925ff6a7dd8cc3b47803c9eb0b89ce57
kamishic <kamishi6@gmail.com>
2020-10-03 20:04:54 +0900

    BASIC認証追加

---
+++ app/controllers/starts_controller.rb
@@ -0,0 +1,5 @@
+class StartsController < ApplicationController
+  def index
+  end
+    
+end
---
+++ test/controllers/starts_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class StartsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
