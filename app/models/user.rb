1991b6ee23ca91758f3ab5b8499bda7a0fe4a1e4
kamishic <kamishi6@gmail.com>
2020-11-17 21:34:01 +0900

    user単体テストコード作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,23 +4,26 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email,format: {with: VALID_EMAIL_REGEX ,message: '：メールアドレスは@マークを入れてご入力ください。' }
+
   VALID_PASSWORD_REGEX = /(?=.*\d+.*)(?=.*[a-zA-Z]+.*)/
-  validates :password,format: {with: VALID_PASSWORD_REGEX ,message: '：パスワードは半角英数字を混合で６文字以上でご入力ください。' }
+  validates :password,format: {with: VALID_PASSWORD_REGEX ,message: '：パスワードは半角英数字混合でご入力ください。' }
 
-  validates :nickname, presence: { message: '：ニックネームをご入力ください' }
+  validates :nickname, presence: { message: '：ニックネームをご入力ください。' }
   
-  VALID_NAME_REGEX = /\A[ぁ-ゔァ-ヴ一-龥]/
-  validates :lastname, presence: { message: '：姓をご入力ください' }
-  validates :lastname,format: {with: VALID_NAME_REGEX ,message: '：姓は全角漢字・ひらがな・カタカナでご入力下さい。' }
-  validates :firstname, presence: { message: '：名を入力してください' }
-  validates :firstname,format: {with: VALID_NAME_REGEX ,message: '：名は全角漢字・ひらがな・カタカナでご入力下さい。' }
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
+  validates :lastname, presence: { message: '：姓をご入力ください。' }
+  validates :lastname,format: {with: VALID_NAME_REGEX ,message: '：姓は全角漢字・ひらがな・カタカナでご入力ください。' }
+  validates :firstname, presence: { message: '：名をご入力ください。' }
+  validates :firstname,format: {with: VALID_NAME_REGEX ,message: '：名は全角漢字・ひらがな・カタカナでご入力ください。' }
 
-  VALID_KANA_REGEX = /\A[ァ-ヴ]/
-  validates :kana_lastname, presence: { message: '：カナ(姓)を入力してください' }
-  validates :kana_lastname,format: {with: VALID_KANA_REGEX ,message: '：カナ(姓)は全角カタカナでご入力下さい。' }
-  validates :kana_firstname, presence: { message: '：カナ(名)を入力してください' }
-  validates :kana_firstname,format: {with: VALID_KANA_REGEX ,message: '：カナ(名)は全角カタカナでご入力下さい。' }
+  VALID_KANA_REGEX = /\A[ァ-ヶー－]+\z/
+  validates :kana_lastname, presence: { message: '：カナ(姓)を入力してください。' }
+  validates :kana_lastname,format: {with: VALID_KANA_REGEX ,message: '：カナ(姓)は全角カタカナでご入力ください。' }
+  validates :kana_firstname, presence: { message: '：カナ(名)を入力してください。' }
+  validates :kana_firstname,format: {with: VALID_KANA_REGEX ,message: '：カナ(名)は全角カタカナでご入力ください。' }
 
-  validates :birthday, presence: { message: '：生年月日を入力してください' }
+  validates :birthday, presence: { message: '：生年月日を入力してください。' }
 
 end
