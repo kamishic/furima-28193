1e94bb0e968f637599080b4c5462df0b8a6ffec4
kamishic <kamishi6@gmail.com>
2020-11-18 15:39:49 +0900

    validation実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -19,7 +19,7 @@ class ProductsController < ApplicationController
 
   private
   def product_params
-    params.require(:product).permit(:content, :image).merge(user_id: current_user.id)
+    params.require(:product).permit(:name, :image, :description, :category_id, :status_id, :cost_ship_id, :area_from_ship_id, :day_to_ship_id, :price).merge(user_id: current_user.id)
   end
 
   def move_to_index
