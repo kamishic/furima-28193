e17f0ef5abae5b72ef8d39f160776e1299580df2
kamishic <kamishi6@gmail.com>
2020-11-18 12:17:06 +0900

    controllerに、未ログインユーザーが出品ページに移動不可制御を追加

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,17 @@
 class ProductsController < ApplicationController
+  before_action :move_to_index,except: [:index]
+  
   def index
   end
+
   def new
   end
+
+  private
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
+
 end
