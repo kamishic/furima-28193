bf7d09c47211dfd6f755e360183a2939870d748c
kamishic <kamishi6@gmail.com>
2020-11-18 12:42:42 +0900

    createメソッドとnewのviewを作成
    ビューの配列箇所はActiveHash後に実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,9 +5,23 @@ class ProductsController < ApplicationController
   end
 
   def new
+    @product = Product.new
+  end
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
+
   private
+  def product_params
+    params.require(:product).permit(:content, :image).merge(user_id: current_user.id)
+  end
+
   def move_to_index
     unless user_signed_in?
       redirect_to action: :index
