cdefa3438e30e4ae17553372a0495dee3e2b67cf
kamishic <kamishi6@gmail.com>
2020-11-19 11:06:41 +0900

    商品情報編集機能_2回目レビュー後修正版

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_product, only: [:show, :edit, :update]
 
   def index
